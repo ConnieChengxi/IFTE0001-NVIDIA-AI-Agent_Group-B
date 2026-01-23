c646dd61cd319779e43593c2da4e465c7923e753
Zehui Zhang <ucesz57@ucl.ac.uk>
2026-01-23 10:45:47 +0000

    Update agent.py

--- agent.py
+++ agent.py
@@ -1,10 +1,11 @@
-from .config import DEFAULT_YEARS
+from .config import DEFAULT_SYMBOL, DEFAULT_YEARS
 from .av_client import fetch_annual_statement_paths
 from .statements import build_annual_dfs, to_views_in_bn
 
 
-def run(symbol: str = "NVDA"):
+def run(symbol: str = DEFAULT_SYMBOL):
     paths = fetch_annual_statement_paths(symbol)
+
     is_df, bs_df, cf_df = build_annual_dfs(paths, DEFAULT_YEARS)
     is_view, bs_view, cf_view = to_views_in_bn(is_df, bs_df, cf_df)
 
@@ -18,4 +19,3 @@ def run(symbol: str = "NVDA"):
         "bs_view": bs_view,
         "cf_view": cf_view,
     }
-
